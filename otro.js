95de81ee07794094ca5dfb32c766ed96e33a5c15
Kanashii <danieltlegamer@gmail.com>
2026-04-30 00:05:02 -0400

    irrelevant archives removed

--- otro.js
+++
@@ -1,5 +0,0 @@
-module.exports = function(app){
-     app.get("/otro",(req,res)=>{
-          res.json({text:"este no es mio! chuchu"});
-     })
-}
--- publico.js
+++
@@ -1,5 +0,0 @@
-module.exports = function(app){
-     app.get("/publico",(req,res)=>{
-          res.json({text:"este es publico, lo veremos todos"});
-     })
-}
